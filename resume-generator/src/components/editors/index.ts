9ab31cd47a2684c89a6e7bddb1c2b289ed9aa7db
Jon Weatherspoon <jweatherspoon.se@gmail.com>
2022-04-24 20:59:27 -0700

    update export file

--- resume-generator/src/components/editors/index.ts
+++ resume-generator/src/components/editors/index.ts
@@ -1,9 +1,15 @@
 import BooleanEditor from './boolean-editor';
+import DateEditor from './date-editor';
+import EnumEditor from './enum-editor';
 import NumericEditor from './numeric-editor';
+import TextEditor from './text-editor';
 
 const editors = {
   BooleanEditor,
   NumericEditor,
+  DateEditor,
+  EnumEditor,
+  TextEditor,
 };
 
 export default editors;
